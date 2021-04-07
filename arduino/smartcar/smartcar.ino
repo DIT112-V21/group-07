5d6782b7a0218e227c4be06f83c10108c3dae28f
Gianmarco Iachella <81154210+iachella@users.noreply.github.com>
2021-04-07 15:39:22 +0200

    Add basic slow down smoothly functionalities
    - added variable safetyDistance in loop() to have a proportion of when to start slowing down related to the car's current speed
    - added slowDownSmoothly() method, to cut the speed of the car by 50% and stop in case of obstacle
    - added convertSpeed() method, to return the speed of the car in percentage

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,84 @@
 #include <Smartcar.h>
 
-void setup() {
-  // put your setup code here, to run once:
+const int TRIGGER_PIN           = 6; // D6
+const int ECHO_PIN              = 7; // D7
+const unsigned int MAX_DISTANCE = 1000;
+const auto pulsesPerMeter = 600;
+const float maxSpeedMs = 1.845;
+
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+SR04 front(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+GY50 gyroscope(arduinoRuntime, 37);
+
+DirectionalOdometer leftOdometer{
+  arduinoRuntime,
+  smartcarlib::pins::v2::leftOdometerPins,
+  []() { leftOdometer.update(); },
+  pulsesPerMeter};
+DirectionalOdometer rightOdometer{
+  arduinoRuntime,
+  smartcarlib::pins::v2::rightOdometerPins,
+  []() { rightOdometer.update(); },
+  pulsesPerMeter};
+
+SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
+
+void setup()
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  float safetyDistance = car.getSpeed() * 200;
+  //Add all sensors!
+  if (front.getDistance() > safetyDistance || front.getDistance()  == 0) {
+    handleInput();
+    Serial.println(front.getDistance());
+    delay(100);
+  } else {
+    slowDownSmoothly();
+  }
+}
+
+void handleInput()
+{
+  if (Serial.available()) {
+
+    String input = Serial.readStringUntil('\n');
+
+    if (input.startsWith("s")) {
+
+      int inputSpeed = input.substring(1).toInt();
+      car.setSpeed(inputSpeed);
+
+    } else if (input.startsWith("a")) {
+
+      int inputAngle = input.substring(1).toInt();
+      car.setAngle(inputAngle);
+
+    }
+  }
+}
+
+void slowDownSmoothly()
+{
+  while (car.getSpeed() > 0) { //while moving forward
+    car.setSpeed(convertSpeed(car.getSpeed())/2);//cut it down by 50%
+    Serial.println(car.getSpeed());
+    if (car.getSpeed() <= 0.2) { //if "slow enough"
+      car.setSpeed(0);//stop
+    }
+  }
+}
+
+float convertSpeed(float currentSpeedMs)
+{
+    float result = (currentSpeedMs/maxSpeedMs)*100;
+    return result;
 }
