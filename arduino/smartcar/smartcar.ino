cadcef3a8cf6fbfb19d301e84096e5df85de3625
chris <c>
2021-04-07 15:51:15 +0200

    Changes made: Check for all sensors values (duplicating code made for the front US)

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -17,11 +17,11 @@ BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
 //Control
 DifferentialControl control(leftMotor, rightMotor);
-//Infrared sensors (all medium - 12 to 78cm)
+//Infrared sensors (all medium - 12 to 78cm) backIR = 25 - 120cm range
 GP2Y0A21 frontIR(arduinoRuntime, FRONT_PIN);
 GP2Y0A21 rightIR(arduinoRuntime, RIGHT_PIN);
 GP2Y0A21 leftIR(arduinoRuntime, LEFT_PIN);
-GP2Y0A21 backIR(arduinoRuntime, BACK_PIN);
+GP2Y0A02 backIR(arduinoRuntime, BACK_PIN);
 //Ultrasonic sensor
 SR04 frontUS(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 //Heading sensor
@@ -49,8 +49,64 @@ void setup()
 
 void loop()
 {
+    //var = newMethod -- returns the problem sensor (first letter) or pins if possible
+    //char sensor = checkSensors();
+    char sensor;
+    if(frontUS.getDistance() < 80 && frontUS.getDistance() > 0){
+        sensor = 'f';
+    }else if (backIR.getDistance() < 70 && backIR.getDistance() > 0){
+        sensor = 'b';
+    }else if (leftIR.getDistance() < 70 && leftIR.getDistance() > 0){
+        sensor = 'l';
+    }else if (rightIR.getDistance() < 70 && rightIR.getDistance() > 0){
+        sensor = 'r';
+    }else{
+        sensor = 'x';
+    }
 
-    if (frontUS.getDistance() > 80 || frontUS.getDistance()  == 0) {
+    switch(sensor){
+        case 'f':
+            { car.setSpeed(0);
+            delay(1000);
+            while(frontUS.getDistance() < 80){
+                car.setSpeed(-10);
+            }};
+            break;
+        case 'b':
+            {car.setSpeed(0);
+            delay(1000);
+            while(backIR.getDistance() < 30 || backIR.getDistance() == 0){
+                car.setSpeed(10);
+            }};
+            break;
+        case 'l':
+            {car.setSpeed(10);
+            delay(1000);
+            while(leftIR.getDistance() < 40){
+                car.setAngle(90);
+            }};
+            break;
+        case 'r':
+            { car.setSpeed(10);
+            delay(1000);
+            while(rightIR.getDistance() < 40){
+                car.setAngle(-90);
+            }};
+            break;
+        case 'x':
+            { handleInput();
+            Serial.println(frontUS.getDistance());
+            //Disabled to not overload the serial console, Enable if you want to see the distance printed.
+            //Serial.println(frontIR.getDistance());
+            //Serial.println(leftIR.getDistance());
+            //Serial.println(rightIR.getDistance());
+            Serial.println(backIR.getDistance());
+            delay(100);
+            }};
+
+    }
+    //replace if statement (switch with letter/pin cases)
+/*    if (frontUS.getDistance() > 80 || frontUS.getDistance()  == 0) {
         handleInput();
         Serial.println(frontUS.getDistance());
         //Disabled to not overload the serial console, Enable if you want to see the distance printed.
@@ -65,25 +121,42 @@ void loop()
         while(front.getDistance() < 80){
             car.setSpeed(-10);
         }
+    }*/
+
+
+void handleInput() {
+    if (Serial.available()) {
+
+        String input = Serial.readStringUntil('\n');
+
+        if (input.startsWith("s")) {
+
+            int inputSpeed = input.substring(1).toInt();
+            car.setSpeed(inputSpeed);
+
+        } else if (input.startsWith("a")) {
+
+            int inputAngle = input.substring(1).toInt();
+            car.setAngle(inputAngle);
+
+        }
     }
 }
 
-void handleInput()
-{
-  if (Serial.available()){
-
-    String input = Serial.readStringUntil('\n');
-
-    if(input.startsWith("s")){
-
-      int inputSpeed = input.substring(1).toInt();
-      car.setSpeed(inputSpeed);
-
-    }else if(input.startsWith("a")){
-
-      int inputAngle = input.substring(1).toInt();
-      car.setAngle(inputAngle);
-
-    }
-  }
-}
+/*    char checkSensors()
+    {
+        char sensor;
+        if(frontUS.getDistance() < 80){
+            sensor = f;
+        }else if (backIR.getDistance() < 70){
+            sensor = b;
+        }else if (leftIR.getDistance() < 70){
+            sensor = l;
+        }else if (rightIR.getDistance() < 70){
+            sensor = r;
+        }else{
+            sensor = x;
+        }
+        return sensor;
+    }*/
+//}
