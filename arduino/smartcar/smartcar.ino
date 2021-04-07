a865d02f104441465fad25d095597e7f21ec0e86
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-04-07 10:05:09 +0200

    Smart car with fullControll and Directionnal Odometers #2

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,9 +1,54 @@
 #include <Smartcar.h>
 
-void setup() {
-  // put your setup code here, to run once:
+ArduinoRuntime arduinoRuntime;
+BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
+BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+DifferentialControl control(leftMotor, rightMotor);
+
+
+GY50 gyroscope(arduinoRuntime, 37);
+
+const auto pulsesPerMeter = 600;
+
+DirectionalOdometer leftOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::leftOdometerPins,
+    []() { leftOdometer.update(); },
+    pulsesPerMeter);
+DirectionalOdometer rightOdometer(
+    arduinoRuntime,
+    smartcarlib::pins::v2::rightOdometerPins,
+    []() { rightOdometer.update(); },
+    pulsesPerMeter);
+
+SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
+
+void setup()
+{
+    Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    handleInput();
+}
+
+void handleInput()
+{
+  if (Serial.available()){
+
+    String input = Serial.readStringUntil('\n');
+
+    if(input.startsWith("s")){
+
+      int inputSpeed = input.substring(1).toInt();
+      car.setSpeed(inputSpeed);
+
+    }else if(input.startsWith("a")){
+
+      int inputAngle = input.substring(1).toInt();
+      car.setAngle(inputAngle);
+
+    }else if(input.
+  }
 }
