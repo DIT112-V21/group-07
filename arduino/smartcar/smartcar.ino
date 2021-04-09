615185901cf323c0d3e78a5e125c8e9e235dbc71
edizgenc2021 <gusgened@student.gu.se>
2021-04-09 18:54:31 +0200

    Basic Obstacle Avoidance is modified

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,15 +1,24 @@
 #include <Smartcar.h>
 
+/*
+Assignment - Basic obstacle avoidance
+"Create simple Arduino sketch that performs basic obstacle avoidance with a Smartcar. Specifically, 
+you need to make the Smartcar move forward until it detects an obstacle at a distance of your choosing."
+
+Upon our understanding over the assignment, we concluded that smartcar move forward directly until it detects
+an obstacle at a distance of our choosing which was 25cm.
+*/
+
 //Pin definition and constants
 const int FRONT_PIN = 0;
 const int LEFT_PIN = 1;
 const int RIGHT_PIN = 2;
 const int BACK_PIN = 3;
-const int TRIGGER_PIN           = 6; // D6
-const int ECHO_PIN              = 7; // D7
+const int TRIGGER_PIN = 6; // D6
+const int ECHO_PIN = 7; // D7
 const unsigned int MAX_DISTANCE = 1000;
 const auto pulsesPerMeter = 600;
-const float maxSpeedMs = 1.845;
+const float maxSpeedMs = 1.845F;
 
 //Runtime environment
 ArduinoRuntime arduinoRuntime;
@@ -30,16 +39,16 @@ SR04 frontUS(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 GY50 gyroscope(arduinoRuntime, 37);
 
 //Directional odometers
-DirectionalOdometer leftOdometer(
+DirectionalOdometer leftOdometer{
     arduinoRuntime,
     smartcarlib::pins::v2::leftOdometerPins,
     []() { leftOdometer.update(); },
-    pulsesPerMeter);
-DirectionalOdometer rightOdometer(
+    pulsesPerMeter};
+DirectionalOdometer rightOdometer{
     arduinoRuntime,
     smartcarlib::pins::v2::rightOdometerPins,
     []() { rightOdometer.update(); },
-    pulsesPerMeter);
+    pulsesPerMeter};
 
 //Constructor of the SmartCar
 SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
@@ -49,7 +58,12 @@ void setup()
     Serial.begin(9600);
 }
 
+// Distance of our choice is 25cm from the wall.
 void loop()
 {
-  
+    car.setSpeed(70);
+    while(!(frontUS.getDistance() == 0) && frontUS.getDistance() < 90 ){
+        car.setSpeed(0);
+        Serial.println(frontUS.getDistance());
+    }
 }
