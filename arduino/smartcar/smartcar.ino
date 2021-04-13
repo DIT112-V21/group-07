5a9379376a71c55008ef4d0122dcaedaec4c1eba
chris <c>
2021-04-13 17:28:11 +0200

    ########################################################## Accident prevention behavior - progress ########################################################## Type: User story Tags: #UserStory, #Sprint1 Changes: - started fresh with assignment 1 code as base - added and changed handleInput() to restrict movement int obstacles - added emergencyBrake() to stop in case of obstacle detection - added reverseFunction() to move away from obstacles to allow adjustments
    Notes:
    - User story edited to make it more clear.
    - All code done by myself and @alrapal AKA Alexandre Rancati-Palmer.

--- arduino/smartcar/smartcar.ino
+++ arduino/smartcar/smartcar.ino
@@ -1,43 +1,51 @@
 #include <Smartcar.h>
 
 //Pin definition and constants
+const int FRONT_STOP_DISTANCE = 100;
+const int BACK_STOP_DISTANCE = 50;
+const int CLEAR_DISTANCE = 0;
 const int FRONT_PIN = 0;
 const int LEFT_PIN = 1;
 const int RIGHT_PIN = 2;
 const int BACK_PIN = 3;
 const int TRIGGER_PIN           = 6; // D6
 const int ECHO_PIN              = 7; // D7
-const unsigned int MAX_DISTANCE = 1000;
-const auto pulsesPerMeter = 600;
+const unsigned int MAX_DISTANCE = 400;
+const auto PULSES_PER_METER = 600;
 
 //Runtime environment
 ArduinoRuntime arduinoRuntime;
+
 //Motors
 BrushedMotor leftMotor(arduinoRuntime, smartcarlib::pins::v2::leftMotorPins);
 BrushedMotor rightMotor(arduinoRuntime, smartcarlib::pins::v2::rightMotorPins);
+
 //Control
 DifferentialControl control(leftMotor, rightMotor);
-//Infrared sensors (all medium - 12 to 78cm) backIR = 25 - 120cm range
-GP2Y0A21 frontIR(arduinoRuntime, FRONT_PIN);
-GP2Y0A21 rightIR(arduinoRuntime, RIGHT_PIN);
-GP2Y0A21 leftIR(arduinoRuntime, LEFT_PIN);
+
+//Infrared sensors (all long range 25 - 120cm range)
+GP2Y0A02 frontIR(arduinoRuntime, FRONT_PIN);
+GP2Y0A02 rightIR(arduinoRuntime, RIGHT_PIN);
+GP2Y0A02 leftIR(arduinoRuntime, LEFT_PIN);
 GP2Y0A02 backIR(arduinoRuntime, BACK_PIN);
+
 //Ultrasonic sensor
 SR04 frontUS(arduinoRuntime, TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
-//Heading sensor
+
+//Header
 GY50 gyroscope(arduinoRuntime, 37);
 
 //Directional odometers
 DirectionalOdometer leftOdometer{
-    arduinoRuntime,
-    smartcarlib::pins::v2::leftOdometerPins,
-    []() { leftOdometer.update(); },
-    pulsesPerMeter};
+        arduinoRuntime,
+        smartcarlib::pins::v2::leftOdometerPins,
+        []() { leftOdometer.update(); },
+        PULSES_PER_METER};
 DirectionalOdometer rightOdometer{
-    arduinoRuntime,
-    smartcarlib::pins::v2::rightOdometerPins,
-    []() { rightOdometer.update(); },
-    pulsesPerMeter};
+        arduinoRuntime,
+        smartcarlib::pins::v2::rightOdometerPins,
+        []() { rightOdometer.update(); },
+        PULSES_PER_METER};
 
 //Constructor of the SmartCar
 SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
@@ -45,55 +53,55 @@ SmartCar car(arduinoRuntime, control, gyroscope, leftOdometer, rightOdometer);
 void setup()
 {
     Serial.begin(9600);
+
+    //car.setSpeed(20); // set the car speed to 20
 }
-//Main loop method.
-// TODO *Alex & Chris* check reaction to obstacles on the sides.
-// TODO *Alex & Chris* change while condition (local var or a bool method (if value is between x, return true) would probably be more efficient).
+
 void loop()
 {
-    char sensor = checkSensors();
-    switch(sensor){
-        case 'f':
-            { car.setSpeed(0);
-            delay(1000);
-            while(frontUS.getDistance() > 0 && frontUS.getDistance() < 80){
-                car.setSpeed(-10);
-            }car.setSpeed(0);};
-            break;
-        case 'b':
-            {car.setSpeed(0);
-            delay(1000);
-            while(backIR.getDistance() > 0 && backIR.getDistance() < 30 ){
-                car.setSpeed(10);
-            }car.setSpeed(0);};
-            break;
-        case 'l':
-            {car.setSpeed(10);
-            delay(1000);
-            while(leftIR.getDistance() > 0 && leftIR.getDistance() < 30 ){
-                car.setAngle(90);
-            }car.setAngle(0);};
-            break;
-        case 'r':
-            { car.setSpeed(10);
-            delay(1000);
-            while(rightIR.getDistance() > 0 && rightIR.getDistance() < 30) {
-                car.setAngle(-90);
-            }car.setAngle(0);};
-            break;
-        case 'x':
-            { handleInput();
-            Serial.println(frontUS.getDistance());
-            //Disabled to not overload the serial console, Enable if you want to see the distance printed.
-            //Serial.println(frontIR.getDistance());
-            //Serial.println(leftIR.getDistance());
-            //Serial.println(rightIR.getDistance());
-            //Serial.println(backIR.getDistance());
-            delay(100);
-            }};
+        handleInput();
+        emergencyBrake();
+}
 
+
+
+/*bool isClear() {
+    int backValue = backIR.getDistance();
+    int frontValue = frontIR.getDistance();
+    int rightValue = rightIR.getDistance();
+    int leftValue = leftIR.getDistance();
+    Serial.println(backValue);
+    return (backValue == 0 && frontValue == 0 && rightValue == 0 && leftValue == 0);
+}*/
+
+void reverseFunction(bool direction){
+    int frontSensorDistance = frontUS.getDistance();
+    int backSensorDistance = backIR.getDistance();
+
+    if(direction){
+        while(frontSensorDistance < FRONT_STOP_DISTANCE && frontSensorDistance > CLEAR_DISTANCE){
+            car.setSpeed(-10);
+            frontSensorDistance = frontUS.getDistance();
+            if (frontSensorDistance >= FRONT_STOP_DISTANCE && frontSensorDistance > CLEAR_DISTANCE){
+                car.setSpeed(0);
+            }
+        }
+    }else{
+        while(backSensorDistance < BACK_STOP_DISTANCE && backSensorDistance > CLEAR_DISTANCE){
+            car.setSpeed(10);
+            backSensorDistance = backIR.getDistance();
+            if (backSensorDistance >= BACK_STOP_DISTANCE && backSensorDistance > CLEAR_DISTANCE){
+                car.setSpeed(0);
+            }
+        }
     }
 
+}
+
+/* 1) Look at direction and angle so we know which sensors we focus on
+ * 2) look at the selected sensors
+ * 3) act accordingly
+ * */
 void handleInput() {
     if (Serial.available()) {
 
@@ -112,25 +120,55 @@ void handleInput() {
         }
     }
 }
-//Method to check which sensor is being affected.
-// TODO *Alex & Chris* char value to be changed to the pin number of each sensor.
-// TODO *Alex & Chris* comparative values (80, 30) to be changed to a calculated value based on the speed (Longer range sensors are needed for this).
-char checkSensors()
-{
-    char sensor;
-    if(frontUS.getDistance() > 0 && frontUS.getDistance() < 80){
-        sensor = 'f';
-    }else if (backIR.getDistance() > 0 && backIR.getDistance() < 30){
-        sensor = 'b';
-    }else if (leftIR.getDistance() > 0 && leftIR.getDistance() < 30){
-        sensor = 'l';
-    }else if (rightIR.getDistance() > 0 && rightIR.getDistance() < 30){
-        sensor = 'r';
+
+void emergencyBrake(){
+    int leftDirection = leftOdometer.getDirection();
+    int rightDirection = rightOdometer.getDirection();
+    bool direction = false;
+
+    if(leftDirection == 1 && rightDirection == 1){
+        int frontSensorDistance = frontUS.getDistance();
+        direction = true;
+        if (frontSensorDistance !=0 ){ // if the sensor has readings ..
+            if ( frontSensorDistance <= FRONT_STOP_DISTANCE ){ // check if the sensor measurement is equal or less than the stopping distance
+                car.setSpeed(0);// stop the car.
+                delay(2000);
+            }
+        }
     }else{
-        sensor = 'x';
+        int backSensorDistance = backIR.getDistance();
+        if (backSensorDistance !=0 ){ // if the sensor has readings ..
+            if ( backSensorDistance <= BACK_STOP_DISTANCE ){ // check if the sensor measurement is equal or less than the stopping distance
+                car.setSpeed(0); // stop the car.
+                delay(2000);
+            }
+        }
     }
-    return sensor;
+    reverseFunction(direction);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
