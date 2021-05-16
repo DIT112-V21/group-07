129538e5a136a06e90a238ec9ece1964cb6d30e6
juliavankirk <juliavankirk@gmail.com>
2021-05-16 19:41:06 +0200

    changed user selection layout

--- UI/app/src/main/java/com/example/pathfinder/Activities/UserSelection.java
+++ UI/app/src/main/java/com/example/pathfinder/Activities/UserSelection.java
@@ -5,11 +5,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.pathfinder.R;
 
 public class UserSelection extends AppCompatActivity {
+
+    RelativeLayout mDriverBtn, mPassengerBtn;
 
     /**
      * initialise the activity (page) based on the given layout xml file saved into the int view variable.
@@ -19,27 +22,32 @@ public class UserSelection extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
-        int view = R.layout.activity_user_selection;
-        setContentView(view);
+        setContentView(R.layout.activity_user_selection);
+
+        mDriverBtn = findViewById(R.id.user_driver);
+        mPassengerBtn = findViewById(R.id.user_passenger);
+
+        /**
+         * Redirect to the driver login page when the button R.id.driverButton is clicked on.
+         * @param view -> R.id.driverButton for this method.
+         */
+        mDriverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), DriverLogin.class));
+            }
+        });
+
+        /**
+         * Redirect to the passenger dedicated page when the button R.id.passengerButton is clicked on.
+         * @param view -> R.id.passengerButton for this method.
+         */
+        mPassengerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), PassengerDashboard.class));
+            }
+        });
     }
 
-    /**
-     * Redirect to the driver login page when the button R.id.driverButton is clicked on.
-     * @param view -> R.id.driverButton for this method.
-     */
-    public void selectTypeDriver(View view) {
-        Log.d("driver","driver user type selected");
-        Intent intent = new Intent(this, DriverLogin.class);
-        startActivity(intent);
-    }
-
-    /**
-     * Redirect to the passenger dedicated page when the button R.id.passengerButton is clicked on.
-     * @param view -> R.id.passengerButton for this method.
-     */
-    public void selectTypePassenger(View view){
-        Log.d("passenger","passenger user type selected");
-        Intent intent = new Intent(this, PassengerDashboard.class);
-        startActivity(intent);
-    }
 }
