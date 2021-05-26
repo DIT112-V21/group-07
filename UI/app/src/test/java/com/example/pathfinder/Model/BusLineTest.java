4db5921d7f5dde14992f97ee9c99815e0594db0c
Alexandre Rancati-Palmer <71485862+alrapal@users.noreply.github.com>
2021-05-26 11:46:29 +0200

    Adapted tests to new functioning

--- UI/app/src/test/java/com/example/pathfinder/Model/BusLineTest.java
+++ UI/app/src/test/java/com/example/pathfinder/Model/BusLineTest.java
@@ -23,7 +23,7 @@ public class BusLineTest {
         String nextStop = busLine.nextStop();
         assertEquals("Sannaplan", nextStop);
         String nextStop2 = busLine.nextStop();
-        assertEquals("Sandarna", nextStop2);
+        assertEquals(BusLine.TERMINUS + "Sandarna", nextStop2);
         String nextStop3 = busLine.nextStop();
         assertEquals(BusLine.TERMINUS, nextStop3);
     }
